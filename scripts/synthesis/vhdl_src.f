4687366852b4a9350c559973a1c9b7ada9d8e2b6
aelhosiny <amr.elhosiny@si-vision.com>
2014-06-22 16:22:05 +0300

    add synthesis list file

---
+++ scripts/synthesis/vhdl_src.f
@@ -0,0 +1,6 @@
+../../source/rtl/final_cpa.vhd
+../../source/rtl/sdn_gen.vhd
+../../source/rtl/pp_rdcn.vhd
+../../source/rtl/pp_gen_rdcn.vhd
+../../source/rtl/multiplier_top.vhd
+../../source/rtl/three2two.vhd
