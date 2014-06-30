c793939d05a74074792875776e511a8118dfb5ff
aelhosiny <amr.elhosiny@si-vision.com>
2014-06-30 16:58:48 +0200

    add xilinx power reports

--- synth/ise/run.sh
+++ synth/ise/run.sh
@@ -11,9 +11,10 @@ export ROOT_DIR=$ROOT_DIR
 listfile=vhdl_src.f
 export listfile=$listfile
 
-xst -ifn synth.xst -ofn $toplevel".srp" 
+#xst -ifn synth.xst -ofn $toplevel".srp" 
 
-ngdbuild -uc multiplier $toplevel".ngc" 
+#ngdbuild -uc multiplier $toplevel".ngc" 
 
-map -p xc7a100t-csg324-3 -w -logic_opt on -ol high -t 1 -xt 0 -r 4 -mt on -lc off -o $toplevel"_map.ncd" $toplevel".ngd" $toplevel".pcf" -timing
-par -mt 4 -ol high $toplevel"_map.ncd" $toplevel".ncd" $toplevel".pcf" -w
+#map -p xc7a100t-csg324-3 -w -logic_opt on -ol high -t 1 -xt 0 -r 4 -mt on -lc off -o $toplevel"_map.ncd" $toplevel".ngd" $toplevel".pcf" -timing
+#par -mt 4 -ol high $toplevel"_map.ncd" $toplevel".ncd" $toplevel".pcf" -w
+xpwr -ol std $toplevel".ncd" $toplevel".pcf" -o $toplevel".pwr"
