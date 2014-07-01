80842e79f5a40979b3d5f1d0342c42a43dc9be9a
aelhosiny <amr.elhosiny@si-vision.com>
2014-07-01 12:03:31 +0200

    add append script

---
+++ source/tb/multiplier_top_tb/append.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+rm -f multiplier.txt multiplicand.txt
+
+cat multiplier_cb.txt > multiplier.txt
+cat multiplier_mat.txt >> multiplier.txt
+
+cat multiplicand_cb.txt > multiplicand.txt
+cat multiplicand_mat.txt >> multiplicand.txt
